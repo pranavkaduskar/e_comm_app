c6e6ae442268e93fdf29130626e0e1b6ce53a33b
Pranav Kaduskar <pranavkaduskar8@gmail.com>
2024-04-16 01:33:59 +0530

    To day 16-04-2024 I started to creating my first Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import "package:e_comm_app/home_page.dart";
+import "package:flutter/material.dart";
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {    
+    return  MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
